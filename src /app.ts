1dad9725b7fad6906150ab38c782d090280a495e
WWWWW-GNA <21ee01056@iitbbs.ac.in>
2025-05-30 23:29:13 +0530

    Update app.ts

--- src /app.ts
+++ src /app.ts
@@ -1,1 +1,17 @@
+import express from "express";
+import cors from "cors";
+import identifyRouter from "./routes/identify";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/identify", identifyRouter);
+
+app.get("/", (_, res) => {
+  res.send("Bitespeed Backend Task API is running");
+});
+
+export default app;
 
